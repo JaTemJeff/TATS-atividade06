4585f183ef704e8f7ea659bc96c2d2d22b5e330c
jatemjeff <jff_94@hotmail.com>
2018-08-23 20:29:16 -0300

    Teste 2 passando

--- src/main/java/com/utfpr/classes/CalculaMediana.java
+++ src/main/java/com/utfpr/classes/CalculaMediana.java
@@ -5,13 +5,36 @@
  */
 package com.utfpr.classes;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Notebook
  */
 public class CalculaMediana {
+    int i;
+    float res = 0;
     
     public double calculaMediana(double vetor[]){
+        Arrays.sort(vetor);
+        if(vetor.length == 0){
+            return 0;
+        }else{
+            if(vetor.length % 2 == 0){
+                int posicao1 = (int) ((vetor.length / 2) - 0.5f);
+                int posicao2 = (int) ((vetor.length / 2) + 0.5f);
+                res = (float) ((vetor[posicao1]+vetor[posicao2])/2);
+                return res;
+            }else{
+                
+                if(vetor.length % 2 != 0){
+                    int posicao = vetor.length / 2;
+                    res =(float) vetor[posicao];
+                    return res;
+                }
+            }
+        }
+        
         return 3;
     }
 }
